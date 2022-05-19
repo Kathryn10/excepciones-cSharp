e476013c9450839f8d07740e23149cd00a816e91
Kathryn10 <105743488+Kathryn10@users.noreply.github.com>
2022-05-19 12:10:25 -0700

    Visual
    excepciones

---
+++ excepciones-cSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
